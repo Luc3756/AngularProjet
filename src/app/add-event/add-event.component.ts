56f78c3a60b37a7f8f591370e2db988aa8a08c47
Luc3756 <mluc020203@gmail.com>
2024-12-16 21:38:08 +0100

    optimisation ajout event

--- src/app/add-event/add-event.component.ts
+++ src/app/add-event/add-event.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { EventService } from '../services/event.service'; 
+import { EventService } from '../services/event.service';
 
 @Component({
   selector: 'app-add-event',
@@ -8,24 +8,36 @@ import { EventService } from '../services/event.service';
   styleUrls: ['./add-event.component.scss']
 })
 export class AddEventComponent implements OnInit {
+  eventForm!: FormGroup;
 
-  eventForm!: FormGroup; 
-
-  constructor(private eventService: EventService) { }  
+  constructor(private eventService: EventService) {}
 
   ngOnInit(): void {
     this.eventForm = new FormGroup({
-      title: new FormControl('', [Validators.required]),  
+      title: new FormControl('', [Validators.required]),
       imageUrl: new FormControl('', [Validators.required]), 
-      description: new FormControl('', [Validators.required])  
+      description: new FormControl('', [Validators.required])
     });
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const relativePath = `assets/images/${file.name}`;
+
+      this.eventForm.get('imageUrl')?.setValue(relativePath);
+    } else {
+      console.error('Aucun fichier sélectionné');
+    }
   }
 
   onSubmit(): void {
     if (this.eventForm.valid) {
       this.eventService.addEvent(this.eventForm.value).then(() => {
         console.log('Événement ajouté avec succès');
-      }).catch((error: any) => { 
+      }).catch((error: any) => {
         console.error('Erreur lors de l\'ajout de l\'événement :', error);
       });
     } else {
